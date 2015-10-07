84c609dbb7786275d555c211a9fcc48ac0f69e44
bleepbloop13 <rbak9218@740s-pl-161.local>
2015-10-07 13:31:18 -0600

    Resolved NPE, Added Display Class

--- src/list/controller/ListController.java
+++ src/list/controller/ListController.java
@@ -1,6 +1,18 @@
 package list.controller;
 
+import java.util.ArrayList;
+
 public class ListController
 {
-
+	private ArrayList<String> firstWords;
+	
+	public ListController()
+	{
+		firstWords = new ArrayList<String>();
+	}
+	
+	public void start()
+	{
+		firstWords.add("");
+	}
 }
--- src/list/controller/ListRunner.java
+++ src/list/controller/ListRunner.java
@@ -2,5 +2,9 @@ package list.controller;
 
 public class ListRunner
 {
-
+	public static void main(String [] args)
+	{
+		ListController myListController = new ListController();
+		myListController.start();
+	}
 }

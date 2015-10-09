c41a303ddc0ea61aa302e2a0021b45e50e5e6928
bleepbloop13 <rbak9218@740s-pl-161.local>
2015-10-09 13:53:52 -0600

    added loop

--- src/list/controller/ListController.java
+++ src/list/controller/ListController.java
@@ -15,7 +15,15 @@ public class ListController
 	
 	public void start()
 	{
-		firstWords.add("");
+		firstWords.add("communism");
+		firstWords.add("lolololololololol");
+		firstWords.add(1, "*CRYING INSANELY*");
+		displayList();
+		firstWords.set(2, "life is riddled with capitilism");
+		displayList();
+		firstWords.remove(0);
+		displayList();
+		showOtherLoop();
 	}
 	
 	private void displayList()
@@ -23,6 +31,13 @@ public class ListController
 		for(int spot = 0; spot < firstWords.size(); spot ++)
 		{
 			myPopups.showList("The contents at " + spot + " are " + firstWords.get(spot));
+		}
+	}
+	private void showOtherLoop()
+	{
+		for(String words : firstWords)
+		{
+			myPopups.showList(words);
 		}
 	}
 }
